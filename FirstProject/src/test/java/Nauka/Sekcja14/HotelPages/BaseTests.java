eb8c7b92f9f592fbff9d4da3e37826e6a3f16e46
Zyluk Piotr <zylukpiotr@gmail.com>
2022-09-28 13:35:21 +0200

    Sekcja 15 - Zakończenie

--- FirstProject/src/test/java/Nauka/Sekcja14/HotelPages/BaseTests.java
+++ FirstProject/src/test/java/Nauka/Sekcja14/HotelPages/BaseTests.java
@@ -1,5 +1,7 @@
 package Nauka.Sekcja14.HotelPages;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -8,7 +10,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import java.time.Duration;
 import java.util.List;
@@ -16,6 +20,20 @@ import java.util.List;
 public class BaseTests {
 
     protected static WebDriver driver;
+    protected static ExtentHtmlReporter htmlReporter;
+    protected static ExtentReports extentReports;
+
+    @BeforeSuite
+    public void beforeSuite(){
+        htmlReporter = new ExtentHtmlReporter("index.html");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(htmlReporter);
+    }
+    @AfterSuite
+    public void afterSuite(){
+        htmlReporter.flush();
+        extentReports.flush();
+    }
     @BeforeMethod
     public void setup() {
         WebDriverManager.chromedriver().setup();
--- FirstProject/src/test/java/Nauka/Sekcja14/Tests/NewHotelSearch.java
+++ FirstProject/src/test/java/Nauka/Sekcja14/Tests/NewHotelSearch.java
@@ -4,6 +4,9 @@ import Nauka.Sekcja14.HotelPages.BaseTests;
 import Nauka.Sekcja14.HotelPages.HotelSearchPage;
 import Nauka.Sekcja14.HotelPages.ResultsPage;
 import Nauka.Sekcja14.Utilis.ExcelReader;
+import Nauka.Sekcja14.Utilis.SeleniumHelper;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -13,19 +16,26 @@ import java.util.List;
 
 public class NewHotelSearch extends BaseTests{
 
+    protected static HotelSearchPage hotelSearchPage;
     @Test(dataProvider = "data")
-    public void newSearchOfHotel(String city, String hotel) throws InterruptedException {
-        HotelSearchPage hotelSearchPage = new HotelSearchPage()
-                .pageSetup()
-                .setCity(city)
-                .setDates("01/01/2024","01/01/2025")
-                .setNumberOfTravellers("4","2")
-                .setSearchButton();
+    public void newSearchOfHotel(String city, String hotel) throws InterruptedException, IOException {
+        ExtentTest test = extentReports.createTest("New Hotel Search");
+        hotelSearchPage = new HotelSearchPage().pageSetup();
+                test.log(Status.PASS,"Page setup done.", SeleniumHelper.getScreenshot(driver));
+        hotelSearchPage.setCity(city);
+                test.log(Status.PASS,"Setting city done.", SeleniumHelper.getScreenshot(driver));
+        hotelSearchPage.setDates("01/01/2024","01/01/2025");
+                test.log(Status.PASS,"Setting dates check-in/out done.", SeleniumHelper.getScreenshot(driver));
+        hotelSearchPage.setNumberOfTravellers("4 ","2 ");
+                test.log(Status.PASS,"Setting number of travellers done.", SeleniumHelper.getScreenshot(driver));
+        hotelSearchPage.setSearchButton();
+                test.log(Status.PASS,"Search button used.", SeleniumHelper.getScreenshot(driver));
         ResultsPage resultsPage = new ResultsPage(driver);
         List<String> hotelNames = resultsPage.getHotelNames();
         System.out.println();
         hotelNames.forEach(System.out::println);
         Assert.assertEquals(hotel, hotelNames.get(0));
+                test.log(Status.PASS, "Asserts complete", SeleniumHelper.getScreenshot(driver));
     }
     @DataProvider
     public Object[][] data() throws IOException {
